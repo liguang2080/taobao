db3aa53ce244198b4e3a70c06529e501e5829958
lg2046 <lg2046@gmail.com>
2012-06-25 15:37:25 +0800

    fix typo error

--- lib/taobao.rb
+++ lib/taobao.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/taobao/version'
 
 require File.dirname(__FILE__) + '/taobao/config'
-require File.dirname(__FILE__) + '/taobao/oauth'
+require File.dirname(__FILE__) + '/taobao/top_oauth'
 require File.dirname(__FILE__) + '/taobao/client'
 
 
