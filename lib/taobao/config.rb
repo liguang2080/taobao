08ab4b55799f7a1cb65ede638ce27d06c051d0fb
lg2046 <lg2046@gmail.com>
2012-08-10 11:46:57 +0800

    bump 0.3.0

--- lib/taobao/config.rb
+++ lib/taobao/config.rb
@@ -8,10 +8,9 @@ module Taobao
       attr_reader :app_key, :app_secret, :redirect_uri
 
       def load_config
-        # filename = "#{Rails.root}/config/taobao.yml"
-        # config = YAML.load(File.open(filename))[Rails.env]
-        # @app_key, @app_secret, @redirect_uri = config['app_key'], config['secret'], config['redirect_uri']
-        @app_key, @app_secret, @redirect_uri = '12673352', 'a4f1e890dc60e65912c69db3c1cff70f', 'http://chihuo100.com/taobao/callback'
+        filename = "#{Rails.root}/config/taobao.yml"
+        config = YAML.load(File.open(filename))[Rails.env]
+        @app_key, @app_secret, @redirect_uri = config['app_key'], config['secret'], config['redirect_uri']
         raise "Please configure your Tencentpay settings in #{filename}." unless @app_key && @app_secret && @redirect_uri
       end
     end
